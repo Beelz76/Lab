eb8758fef8becf46885be2c148117787cb82ad35
Beelz76 <91071691+Beelz76@users.noreply.github.com>
2021-09-21 21:29:54 +0300

    Add files via upload

---
+++ L 6/6.7.cpp
@@ -0,0 +1,28 @@
+﻿// A^15
+
+#include <locale.h>
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int A,b,c;
+    
+    cout << "A: ";
+    cin >> A;
+
+    b = A * A;
+    c = b * A;
+    A = c * c;
+    A = A * A;
+    A = A * c; 
+
+    cout << "A^15 = " << A << endl;
+
+ 
+    system("pause");
+    return 0;
+
+}
+
