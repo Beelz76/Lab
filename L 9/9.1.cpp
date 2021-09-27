8d958d415932e2043e88e17d525c40a739fe6142
Beelz76 <91071691+Beelz76@users.noreply.github.com>
2021-09-27 17:59:50 +0300

    Add files via upload

---
+++ L 9/9.1.cpp
@@ -0,0 +1,20 @@
+﻿//С начала суток прошло N секунд (N — целое). Найти количество секунд, прошедших с начала последней минуты.
+
+#include <iostream> 
+#include <locale.h>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int N;
+    cin >> N;
+ 
+    cout << "С момента последней минуты прошло: " << N % 60 << "с" << endl;
+
+    system("pause");
+    return 0;
+}
+
+
+
---
+++ L 9/9.5.cpp
@@ -0,0 +1,20 @@
+﻿/*Дан номер некоторого года (целое положительное число). Определить соответствующий 
+  ему номер столетия, учитывая, что, к примеру, началом 20 столетия был 1901 год*/
+
+#include <iostream> 
+#include <locale.h>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int K;
+    cin >> K;
+    
+        cout <<"Номер столетия: "<< (K - 1) / 100 + 1 << endl;
+    
+    system("pause");
+    return 0;
+}
+
+
