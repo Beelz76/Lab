547bb4ee1ade2f290d266a232886a07e7ed5de9f
Beelz76 <91071691+Beelz76@users.noreply.github.com>
2021-09-29 13:49:47 +0300

    Add files via upload

---
+++ L 10/10.5.cpp
@@ -0,0 +1,23 @@
+﻿//Дано четырехзначное число. Проверить истинность высказывания: «Данное число читается одинаково слева направо и справа налево».
+#include <iostream> 
+#include <locale.h>
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int A;
+    std::cin >> A;
+    int a = A % 10;
+    int b = A / 10 % 10;
+    int c = A / 100 % 10;
+    int d = A / 1000 % 10;
+    if (a == d && b == c) {
+        std::cout << "Данное число является палиндромом\n";
+    }
+    else {
+        std::cout << "Данное число не является палиндромом\n";
+    }
+    system("pause");
+    return 0;
+}
+
+
---
+++ L 10/10.6.cpp
@@ -0,0 +1,23 @@
+﻿/*Даны целые числа a, b, c, являющиеся сторонами некоторого треугольника.
+  Проверить истинность высказывания: «Треугольник со сторонами a, b, c является прямоугольным».*/
+#include <iostream> 
+#include <locale.h>
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a,b,c;
+    std::cin >> a;
+    std::cin >> b;
+    std::cin >> c;
+    if (a * a == b * b + c * c || b * b == a * a + c * c || c * c == a * a + b * b) {
+        std::cout << "Треугольник является прямоугольным\n";
+    }
+    else {
+        std::cout << "Треугольник не является прямоугольным\n";
+    }
+   
+    system("pause");
+    return 0;
+}
+
+
