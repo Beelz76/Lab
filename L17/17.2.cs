c57b6e18982860ff27f4a8c6a4f0aa1d186bae54
Beelz76 <91071691+Beelz76@users.noreply.github.com>
2021-10-25 15:49:51 +0300

    Update 17.2.cs

--- L17/17.2.cs
+++ L17/17.2.cs
@@ -9,15 +9,17 @@ namespace main
 
         static void Main(string[] args)
         {
-            int N = Convert.ToInt32(Console.ReadLine());
+            int N =Int32.Parse(Console.ReadLine());
             int[] mas = new int[N];
             int f = 1;
+          
             for (int i = 0; i < N; i++)
             {
-                mas[i] = Convert.ToInt32(Console.ReadLine());
+                mas[i] =Int32.Parse(Console.ReadLine());
             }
 
             int d = mas[1] - mas[0];
+          
             for (int j = 2; j < N; j++)
             {
                 if (mas[j] - mas[j - 1] != d)
@@ -26,6 +28,7 @@ namespace main
                     break;
                 }
             }
+          
             if (f == 1)
             {
                 Console.WriteLine("Разность прогрессии = " + d);
