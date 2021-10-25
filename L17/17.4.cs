149dba2dd2e9538959468de1fe05de9cfae3998a
Beelz76 <91071691+Beelz76@users.noreply.github.com>
2021-10-25 15:51:10 +0300

    Update 17.4.cs

--- L17/17.4.cs
+++ L17/17.4.cs
@@ -9,11 +9,11 @@ namespace main
 
         static void Main(string[] args)
         {
-            int N = Convert.ToInt32(Console.ReadLine());
+            int N =Int32.Parse(Console.ReadLine());
             int[] mas = new int[N];
             int index_lokmax = 0;
 
-            Random r = new Random();
+            var r = new Random();
             for (int i = 0; i < N; i++)
             {
                 mas[i] = r.Next(-10,100) ;
